2755be1c0f2e15576dfc532ab543fb24e68f9481
TheWolfA2 <wolfington98@gmail.com>
2016-10-11 13:59:02 -0700

    Get registrations from Orion

--- OrionApiSdk/ApiEndpoints/Portfolio/RegistrationsMethods.cs
+++ OrionApiSdk/ApiEndpoints/Portfolio/RegistrationsMethods.cs
@@ -1,4 +1,6 @@
-﻿using OrionApiSdk.Objects;
+﻿using Newtonsoft.Json.Linq;
+using OrionApiSdk.Objects;
+using OrionApiSdk.Objects.Portfolio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,56 @@ namespace OrionApiSdk.ApiEndpoints.Portfolio
     {
         public RegistrationsMethods(AuthToken token) : base("Portfolio", "Registrations", token) { }
 
+        #region Get registrations
+        public List<Registration> Get(bool? isActive = null, int? representativeId = null, int top = 5000, int skip = 0)
+        {
+            return GetAsync(isActive, representativeId, top, skip).Result;
+        }
+        public async Task<List<Registration>> GetAsync(bool? isActive = null, int? representativeId = null, int top = 5000, int skip = 0)
+        {
+            JToken registrations = await GetJsonAsync("", new Dictionary<string, object>
+            {
+                { "isActive", isActive },
+                { "representativeId", representativeId },
+                { "$top", top },
+                { "$skip", skip }
+            });
+            return registrations.ToObject<List<Registration>>();
+        }
 
+        public Registration Get(int registrationId)
+        {
+            return GetAsync(registrationId).Result;
+        }
+        public async Task<Registration> GetAsync(int registrationId)
+        {
+            JToken registration = await GetJsonAsync(registrationId.ToString());
+            return registration.ToObject<Registration>();
+        }
+
+        public List<RegistrationSimple> GetSimple(int top = 5000, int skip = 0)
+        {
+            return GetSimpleAsync(top, skip).Result;
+        }
+        public async Task<List<RegistrationSimple>> GetSimpleAsync(int top = 5000, int skip = 0)
+        {
+            JToken registations = await GetJsonAsync("Simple", new Dictionary<string, object>
+            {
+                { "$top", top },
+                { "$skip", skip }
+            });
+            return registations.ToObject<List<RegistrationSimple>>();
+        }
+
+        public RegistrationSimple GetSimple(int registrationId)
+        {
+            return GetSimpleAsync(registrationId).Result;
+        }
+        public async Task<RegistrationSimple> GetSimpleAsync(int registrationId)
+        {
+            JToken registration = await GetJsonAsync(string.Format("{0}/Simple", registrationId));
+            return registration.ToObject<RegistrationSimple>();
+        }
+        #endregion
     }
 }
---
+++ OrionApiSdk/Objects/Portfolio/RegistrationSimple.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using OrionApiSdk.Objects.Abstract;
+
+namespace OrionApiSdk.Objects.Portfolio
+{
+    public class RegistrationSimple : BaseSimpleEntity
+    {
+        [JsonProperty("isActive")]
+        public bool IsActive { get; set; }
+    }
+}
