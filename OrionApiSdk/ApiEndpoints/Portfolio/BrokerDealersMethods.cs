fd89c8743702fdb5dfd69a32313e68cb16d2466e
TheWolfA2 <wolfington98@gmail.com>
2016-10-11 10:39:28 -0700

    simpe search and get for BD

--- OrionApiSdk/ApiEndpoints/Portfolio/BrokerDealersMethods.cs
+++ OrionApiSdk/ApiEndpoints/Portfolio/BrokerDealersMethods.cs
@@ -10,7 +10,7 @@ namespace OrionApiSdk.ApiEndpoints.Portfolio
     {
         public BrokerDealersMethods(AuthToken token) : base("Portfolio", "BrokerDealers", token) { }
 
-        public List<BrokerDealer> Get(int top = 500, int skip = 1)
+        public List<BrokerDealer> Get(int top = 500, int skip = 0)
         {
             return GetAsync(top, skip).Result;
         }
@@ -24,6 +24,40 @@ namespace OrionApiSdk.ApiEndpoints.Portfolio
             return dealers.ToObject<List<BrokerDealer>>();
         }
 
+        public BrokerDealer Get(int bdId)
+        {
+            return GetAsync(bdId).Result;
+        }
+        public async Task<BrokerDealer> GetAsync(int bdId)
+        {
+            JToken brokerDealer = await GetJsonAsync(bdId.ToString());
+            return brokerDealer.ToObject<BrokerDealer>();
+        }
 
+        public List<BrokerDealerSimple> GetSimple(bool? hasValue = null)
+        {
+            return GetSimpleAsync().Result;
+        }
+        public async Task<List<BrokerDealerSimple>> GetSimpleAsync(bool? hasValue = null)
+        {
+            JToken brokerDealers = await GetJsonAsync("", new Dictionary<string, object>
+            {
+                { "hasValue", hasValue }
+            });
+            return brokerDealers.ToObject<List<BrokerDealerSimple>>();
+        }
+
+        public List<BrokerDealerSimple> GetSimpleSearch(string startsWith)
+        {
+            return GetSimpleSearchAsync(startsWith).Result;
+        }
+        public async Task<List<BrokerDealerSimple>> GetSimpleSearchAsync(string startsWith)
+        {
+            JToken dealers = await GetJsonAsync("Simple/Search", new Dictionary<string, object>
+            {
+                { "search", startsWith }
+            });
+            return dealers.ToObject<List<BrokerDealerSimple>>();
+        }
     }
 }
