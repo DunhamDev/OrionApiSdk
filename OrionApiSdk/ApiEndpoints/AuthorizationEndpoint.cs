abf2ee18f8ceb7c9a41d48ea375601a0a51d4bd6
TheWolfA2 <wolfington98@gmail.com>
2016-10-12 09:20:59 -0700

    AuthorizationEndpoint documented

--- OrionApiSdk/ApiEndpoints/AuthorizationEndpoint.cs
+++ OrionApiSdk/ApiEndpoints/AuthorizationEndpoint.cs
@@ -7,14 +7,26 @@ namespace OrionApiSdk.ApiEndpoints
 {
     public class AuthorizationEndpoint : ApiEndpointBase
     {
+        /// <summary>
+        /// Constructs an /Authorization endpoint instance
+        /// </summary>
+        /// <param name="token">The access token of the user making requests</param>
         public AuthorizationEndpoint(AuthToken token) : base("Authorization", token)
         {
         }
 
+        /// <summary>
+        /// Gets the user's profile information
+        /// </summary>
+        /// <returns>The user's profile</returns>
         public UserProfile User()
         {
             return UserAsync().Result;
         }
+        /// <summary>
+        /// Gets the user's profile information
+        /// </summary>
+        /// <returns>The user's profile</returns>
         public async Task<UserProfile> UserAsync()
         {
             JToken userJson = await GetJsonAsync("User");
