f1b7edd78776bb661f79511848a6aa8e0e0f857b
TheWolfA2 <wolfington98@gmail.com>
2016-10-12 10:58:13 -0700

    BD methods documented

--- OrionApiSdk/ApiEndpoints/Portfolio/BrokerDealersMethods.cs
+++ OrionApiSdk/ApiEndpoints/Portfolio/BrokerDealersMethods.cs
@@ -9,12 +9,31 @@ namespace OrionApiSdk.ApiEndpoints.Portfolio
 {
     public class BrokerDealersMethods : ApiMethodContainer
     {
+        /// <summary>
+        /// Constructs an instance of a /Portfolio/BrokerDealers endpoint instance
+        /// </summary>
+        /// <param name="token">The access token of the user making requests</param>
         public BrokerDealersMethods(AuthToken token) : base("Portfolio", "BrokerDealers", token) { }
 
+        #region Get BDs
+        /// <summary>
+        /// HTTP GET: /Portfolio/BrokerDealers
+        /// Gets a list of Broker-Dealers available to the user
+        /// </summary>
+        /// <param name="top">Number of BDs to return</param>
+        /// <param name="skip">Number of BDs to skip</param>
+        /// <returns>A list of Broker-Dealers</returns>
         public List<BrokerDealer> Get(int top = 500, int skip = 0)
         {
             return GetAsync(top, skip).Result;
         }
+        /// <summary>
+        /// HTTP GET: /Portfolio/BrokerDealers
+        /// Gets a list of Broker-Dealers available to the user
+        /// </summary>
+        /// <param name="top">Number of BDs to return</param>
+        /// <param name="skip">Number of BDs to skip</param>
+        /// <returns>A list of Broker-Dealers</returns>
         public async Task<List<BrokerDealer>> GetAsync(int top = 500, int skip = 0)
         {
             JToken dealers = await GetJsonAsync("", new Dictionary<string, object>
@@ -25,20 +44,44 @@ namespace OrionApiSdk.ApiEndpoints.Portfolio
             return dealers.ToObject<List<BrokerDealer>>();
         }
 
+        /// <summary>
+        /// HTTP GET: /Portfolio/BrokerDealers/{bdId}
+        /// Gets a specific Broker-Dealer
+        /// </summary>
+        /// <param name="bdId">ID of the Broker-Dealer to retrieve</param>
+        /// <returns>The specified Broker-Dealer</returns>
         public BrokerDealer Get(int bdId)
         {
             return GetAsync(bdId).Result;
         }
+        /// <summary>
+        /// HTTP GET: /Portfolio/BrokerDealers/{bdId}
+        /// Gets a specific Broker-Dealer
+        /// </summary>
+        /// <param name="bdId">ID of the Broker-Dealer to retrieve</param>
+        /// <returns>The specified Broker-Dealer</returns>
         public async Task<BrokerDealer> GetAsync(int bdId)
         {
             JToken brokerDealer = await GetJsonAsync(bdId.ToString());
             return brokerDealer.ToObject<BrokerDealer>();
         }
 
+        /// <summary>
+        /// HTTP GET: /Portfolio/BrokerDealers/Simple
+        /// Gets a simple list of Broker-Dealers available to the user
+        /// </summary>
+        /// <param name="hasValue"></param>
+        /// <returns>The list of Broker-Dealers</returns>
         public List<BrokerDealerSimple> GetSimple(bool? hasValue = null)
         {
             return GetSimpleAsync().Result;
         }
+        /// <summary>
+        /// HTTP GET: /Portfolio/BrokerDealers/Simple
+        /// Gets a simple list of Broker-Dealers available to the user
+        /// </summary>
+        /// <param name="hasValue"></param>
+        /// <returns>The list of Broker-Dealers</returns>
         public async Task<List<BrokerDealerSimple>> GetSimpleAsync(bool? hasValue = null)
         {
             JToken brokerDealers = await GetJsonAsync("", new Dictionary<string, object>
@@ -47,11 +90,25 @@ namespace OrionApiSdk.ApiEndpoints.Portfolio
             });
             return brokerDealers.ToObject<List<BrokerDealerSimple>>();
         }
+        #endregion
 
+        #region Search BDs
+        /// <summary>
+        /// HTTP GET: /Portfolio/BrokerDealers/Simple/Search
+        /// Gets a simple list of Broker-Dealers available to the user
+        /// </summary>
+        /// <param name="startsWith">Partial Broker-Dealer name to search for</param>
+        /// <returns>A list of Broker-Dealers matching the search</returns>
         public List<BrokerDealerSimple> GetSimpleSearch(string startsWith)
         {
             return GetSimpleSearchAsync(startsWith).Result;
         }
+        /// <summary>
+        /// HTTP GET: /Portfolio/BrokerDealers/Simple/Search
+        /// Gets a simple list of Broker-Dealers available to the user
+        /// </summary>
+        /// <param name="startsWith">Partial Broker-Dealer name to search for</param>
+        /// <returns>A list of Broker-Dealers matching the search</returns>
         public async Task<List<BrokerDealerSimple>> GetSimpleSearchAsync(string startsWith)
         {
             JToken dealers = await GetJsonAsync("Simple/Search", new Dictionary<string, object>
@@ -60,11 +117,25 @@ namespace OrionApiSdk.ApiEndpoints.Portfolio
             });
             return dealers.ToObject<List<BrokerDealerSimple>>();
         }
+        #endregion
 
+        #region BD value
+        /// <summary>
+        /// HTTP GET: /Portfolio/BrokerDealers/Value
+        /// Gets the Broker-Dealers and their values
+        /// </summary>
+        /// <param name="hasValue"></param>
+        /// <returns>The list of Broker-Dealer values</returns>
         public List<BrokerDealerSimple> GetValue(bool? hasValue = null)
         {
             return GetValueAsync(hasValue).Result;
         }
+        /// <summary>
+        /// HTTP GET: /Portfolio/BrokerDealers/Value
+        /// Gets the Broker-Dealers and their values
+        /// </summary>
+        /// <param name="hasValue"></param>
+        /// <returns>The list of Broker-Dealer values</returns>
         public async Task<List<BrokerDealerSimple>> GetValueAsync(bool? hasValue = null)
         {
             JToken dealerValues = await GetJsonAsync("Value", new Dictionary<string, object>
@@ -74,10 +145,24 @@ namespace OrionApiSdk.ApiEndpoints.Portfolio
             return dealerValues.ToObject<List<BrokerDealerSimple>>();
         }
 
+        /// <summary>
+        /// HTTP GET: /Portfolio/BrokerDealers/Value/{asOfDate}
+        /// Gets the Broker-Dealers and their values as of a specific date
+        /// </summary>
+        /// <param name="asOfDate">The specific date</param>
+        /// <param name="hasValue"></param>
+        /// <returns>The list of Broker-Dealer values as of the given date</returns>
         public List<BrokerDealerSimple> GetValue(DateTime asOfDate, bool? hasValue = null)
         {
             return GetValueAsync(asOfDate, hasValue).Result;
         }
+        /// <summary>
+        /// HTTP GET: /Portfolio/BrokerDealers/Value/{asOfDate}
+        /// Gets the Broker-Dealers and their values as of a specific date
+        /// </summary>
+        /// <param name="asOfDate">The specific date</param>
+        /// <param name="hasValue"></param>
+        /// <returns>The list of Broker-Dealer values as of the given date</returns>
         public async Task<List<BrokerDealerSimple>> GetValueAsync(DateTime asOfDate, bool? hasValue = null)
         {
             JToken dealerValues = await GetJsonAsync(string.Format("Value/{0:MM-dd-yyyy}", asOfDate), new Dictionary<string, object>
@@ -86,5 +171,6 @@ namespace OrionApiSdk.ApiEndpoints.Portfolio
             });
             return dealerValues.ToObject<List<BrokerDealerSimple>>();
         }
+        #endregion
     }
 }
