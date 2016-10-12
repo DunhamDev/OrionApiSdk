f303e18830f694750ad0c12d966142561ee1b652
TheWolfA2 <wolfington98@gmail.com>
2016-10-12 09:19:47 -0700

    OrionApi members documented

--- OrionApiSdk/OrionApi.cs
+++ OrionApiSdk/OrionApi.cs
@@ -13,9 +13,18 @@ namespace OrionApiSdk
 {
     public class OrionApi
     {
+        /// <summary>
+        /// The access token for the user making the API requests
+        /// </summary>
         public AuthToken AuthToken { get; private set; }
 
+        /// <summary>
+        /// Endpoint object behind <see cref="Authorization"/> 
+        /// </summary>
         private AuthorizationEndpoint _authorizationEndpoint;
+        /// <summary>
+        /// /Authorization endpoint methods
+        /// </summary>
         public AuthorizationEndpoint Authorization
         {
             get
@@ -24,7 +33,13 @@ namespace OrionApiSdk
             }
         }
 
+        /// <summary>
+        /// Endpoint object behind <see cref="Portfolio"/> 
+        /// </summary>
         private PortfolioEndpoint _portfolioEndpoint;
+        /// <summary>
+        /// /Portfolio endpoint methods
+        /// </summary>
         public PortfolioEndpoint Portfolio
         {
             get
@@ -33,7 +48,13 @@ namespace OrionApiSdk
             }
         }
 
+        /// <summary>
+        /// Endpoint object behind <see cref="Security"/> 
+        /// </summary>
         private SecurityEndpoint _securityEndpoint;
+        /// <summary>
+        /// /Security endpoint methods
+        /// </summary>
         public SecurityEndpoint Security
         {
             get
@@ -42,7 +63,13 @@ namespace OrionApiSdk
             }
         }
 
+        /// <summary>
+        /// Endpoint object behind <see cref="Trading"/> 
+        /// </summary>
         private TradingEndpoint _tradingEndpoint;
+        /// <summary>
+        /// /Trading endpoint methods
+        /// </summary>
         public TradingEndpoint Trading
         {
             get
@@ -51,24 +78,48 @@ namespace OrionApiSdk
             }
         }
 
+        /// <summary>
+        /// Constructs an OrionApi instance
+        /// </summary>
+        /// <param name="token">The authorization token for the user making API requests</param>
         public OrionApi(AuthToken token)
         {
             AuthToken = token;
         }
 
+        /// <summary>
+        /// Authorizies the given user credentials with Orion to retrieve an access token
+        /// </summary>
+        /// <param name="username">The user's Orion username</param>
+        /// <param name="password">The user's Orion password</param>
+        /// <returns>The user's access token</returns>
         public static AuthToken GetUserAuthToken(string username, string password)
         {
             return GetUserAuthTokenAsync(username,password).Result;
         }
+        /// <summary>
+        /// Authorizies the given user credentials with Orion to retrieve an access token
+        /// </summary>
+        /// <param name="username">The user's Orion username</param>
+        /// <param name="password">The user's Orion password</param>
+        /// <returns>The user's access token</returns>
         public static async Task<AuthToken> GetUserAuthTokenAsync(string username, string password)
         {
             return await SecurityEndpoint.TokenAsync(username, password);
         }
 
+        /// <summary>
+        /// Attempts to see if the OrionApi instance's access token is still valid
+        /// </summary>
+        /// <returns>True iff the access token is valid, otherwise false</returns>
         public bool IsAuthTokenValid()
         {
             return IsAuthTokenValidAsync().Result;
         }
+        /// <summary>
+        /// Attempts to see if the OrionApi instance's access token is still valid
+        /// </summary>
+        /// <returns>True iff the access token is valid, otherwise false</returns>
         public async Task<bool> IsAuthTokenValidAsync()
         {
             UserProfile user = await Authorization.UserAsync();
