224aeff8166df419d1b9031db745aa62a745ee89
TheWolfA2 <wolfington98@gmail.com>
2016-10-19 09:09:45 -0700

    Added Owin NuGet packages, and created authentication options

--- OrionApiSdk/Owin/Providers/Orion/OrionAuthenticationExtensions.cs
+++ OrionApiSdk/Owin/Providers/Orion/OrionAuthenticationExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace OrionApiSdk.Owin.Providers.Orion
 {
-    public class OrionAuthenticationExtensions
+    public static class OrionAuthenticationExtensions
     {
+        public static IAppBuilder UseOrionAuthentication(this IAppBuilder app, OrionAuthenticationOptions options)
+        {
+            return app.Use(typeof(OrionAuthenticationMiddleware), app, options);
+        }
     }
 }
--- OrionApiSdk/Owin/Providers/Orion/OrionAuthenticationOptions.cs
+++ OrionApiSdk/Owin/Providers/Orion/OrionAuthenticationOptions.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,27 @@ using System.Threading.Tasks;
 
 namespace OrionApiSdk.Owin.Providers.Orion
 {
-    public class OrionAuthenticationOptions
+    public class OrionAuthenticationOptions : AuthenticationOptions
     {
+        #region Instance properties
+        public PathString CallbackPath { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string SignInAsAutenticationType { get; set; }
+
+        public ISecureDataFormat<AuthenticationProperties> StateDataFormat { get; set; }
+        #endregion
+
+        public OrionAuthenticationOptions(string clientId, string clientSecret) : base("Orion")
+        {
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+            CallbackPath = new PathString("/signin-orion");
+            Description.Caption = "Orion";
+            AuthenticationMode = AuthenticationMode.Passive;
+        }
     }
 }
