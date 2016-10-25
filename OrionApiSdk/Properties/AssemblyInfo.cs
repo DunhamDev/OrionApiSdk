356ef30bc7a9b1b756abe8a627984cc15b2211f1
TheWolfA2 <wolfington98@gmail.com>
2016-10-25 12:24:26 -0700

    Set up Nuget package manager deployment

--- OrionApiSdk/Properties/AssemblyInfo.cs
+++ OrionApiSdk/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OrionApiLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("OrionApiSdk")]
+[assembly: AssemblyDescription("Client library for easy interfacing with api.orionadvisor.com")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OrionApiLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Dunham and Associates, Inc.")]
+[assembly: AssemblyProduct("OrionApiSdk")]
+[assembly: AssemblyCopyright("Copyright 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
